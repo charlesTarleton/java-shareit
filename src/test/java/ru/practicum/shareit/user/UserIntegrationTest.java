930d5431d5e361049a448e9e3d9adb62db043ee4
Rustam N <charlsetarleton@yandex.ru>
2023-08-22 13:54:27 +0300

    Первая итерация ТЗ 15

--- src/test/java/ru/practicum/shareit/user/UserIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/user/UserIntegrationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
